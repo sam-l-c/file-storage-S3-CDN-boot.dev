2001f72d09300b566cd28a24ae3b85edca5a21e5
sam-l-c <samuel.larouche-cote.1@ulaval.ca>
2026-06-18 20:23:33 -0400

    chapter 1 finished

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +32,62 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse request", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "incompatible file type", err)
+		return
+	}
+
+	extensions, err := mime.ExtensionsByType(mediaType)
+	ext := extensions[0]
+
+	filePath := filepath.Join(cfg.assetsRoot, videoIDString+ext)
+	// fmt.Printf("\n\vvideoIDString!!!!!!!!!!!!!!!!!!!!!: %s\n", videoIDString)
+	// fmt.Printf("\n\nfilepath!!!!!!!!!!!!!!!!!!!!!: %s\n", filePath)
+
+	/*
+		data, err := io.ReadAll(file)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "unable to parse thumbnail", err)
+			return
+		}
+	*/
+	dst, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create file", err)
+		return
+	}
+	defer dst.Close()
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create destination file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to retrieve video data", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/%s", cfg.port, filePath)
+	video.ThumbnailURL = &thumbnailURL
+	cfg.db.UpdateVideo(video)
+
+	fmt.Printf("\n\ntumbnailURL:\n%s", thumbnailURL)
+
+	respondWithJSON(w, http.StatusOK, video)
 }
